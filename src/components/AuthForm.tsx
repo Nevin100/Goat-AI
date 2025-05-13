35f96b2352566eb50a81badfe2dbd3bdb55cd6e3
Nevin-Bali100 <opopopopop100000@gmail.com>
2025-05-13 09:56:06 +0530

    Completing Login Page UI

--- src/components/AuthForm.tsx
+++ src/components/AuthForm.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 "use client"
 
-import React, { useState, useTransition } from 'react'
+import React, { useTransition } from 'react'
 import { useRouter } from 'next/navigation';
 import { toast } from "sonner";
 import { CardContent, CardFooter } from './ui/card';
@@ -27,7 +27,7 @@ const AuthForm = ({type} : Props) => {
     <div>
       <form action={handleSubmit}>
         <CardContent className="grid w-full items-center gap-8">
-          <div className="flex flex-col gap-3.5">
+          <div className="flex flex-col gap-1.5">
             <Label htmlFor="email" className='text-lg'>Email :</Label>
             <Input
               type="email"
@@ -37,10 +37,10 @@ const AuthForm = ({type} : Props) => {
               disabled={isPending}
               className="w-full py-4 my-1 border rounded-sm focus:outline-none focus:ring-2 focus:ring-blue-500" />
           </div>
-          <div className="flex flex-col gap-3.5">
-            <Label htmlFor="email" className='text-lg'>Password :</Label>
+          <div className="flex flex-col gap-1.5">
+            <Label htmlFor="Password" className='text-lg'>Password :</Label>
             <Input
-              type="Pawssword"
+              type="password"
               name="Password"
               id="Password"
               placeholder="Type your Password"
@@ -48,12 +48,12 @@ const AuthForm = ({type} : Props) => {
               className="w-full py-4 my-1 border rounded-sm focus:outline-none focus:ring-2 focus:ring-blue-500" />
           </div>
         </CardContent>
-        <CardFooter className='flex flex-col gap-4 mt-8'>
+        <CardFooter className='flex flex-col gap-5 mt-8'>
           <Button className='w-full hover:cursor-pointer my-1 py-5 text-lg'>
             {isPending ? <Loader2 className='animate-spin'/> : isLoginForm ? 'Login' : 'Sign Up'}
           </Button>
-          <p className='text-md'>{isLoginForm ? ("Don't have an account yet?? ") : ("Already have an Account")}
-            <Link href={isLoginForm ? "/sign-up" : "/login"} className='text-md'>{ isLoginForm ? "SignUp" : "LogIn"}</Link>
+          <p className='text-md'>{isLoginForm ? ("Don't have an account yet?? ") : ("Already have an Account")}{" "}
+            <Link href={isLoginForm ? "/sign-up" : "/login"} className='text-md text-blue-400'>{ isLoginForm ? "SignUp" : "LogIn"}</Link>
           </p>
 
         </CardFooter>

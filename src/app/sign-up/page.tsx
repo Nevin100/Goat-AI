c5c731d15e86e7f75c574a61cb6e620c90d8802e
Nevin-Bali100 <opopopopop100000@gmail.com>
2025-05-13 10:00:02 +0530

    Completing Sign Up UI

---
+++ src/app/sign-up/page.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+
+import AuthForm from '@/components/AuthForm';
+const Login = () => {
+  return (
+    <div className='mt-10 flex flex-1 flex-col items-center'>
+      <Card className='w-full max-w-md'> 
+        <CardHeader className='mb-4'>
+          <CardTitle className='text-center text-3xl font-semibold'>Sign Up </CardTitle>
+          {/* <CardDescription className='text-center text-sm'>Login to your account</CardDescription>   */}
+        </CardHeader>
+
+        <AuthForm type='signup'/>
+      </Card>
+    </div>
+  )
+}
+
+export default Login
